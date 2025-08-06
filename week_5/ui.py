f304f0e0fbd6873b4539b4812398d9f2ea614500
chiraphatnim <jinaphat1483@gmail.com>
2025-08-06 14:19:04 +0700

    week_6

--- week_5/ui.py
+++ week_5/ui.py
@@ -4,7 +4,7 @@ import serial
 from  PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout,QMessageBox
 
 try :
-  arduino = serial.Serial('COM3',9600, timeout=1)
+    
 except Exception as e:
   arduino = None
   print("เชื่อต่อไม่ได้: ",e)
